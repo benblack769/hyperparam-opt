fa25b969b292383ba892fa5dc57cfa9cfbe9bedc
ben black <weepingwillowben@gmail.com>
2019-12-23 09:01:12 -0700

    handling of discrete variables by one-hot vectors

--- init_optimizer.py
+++ init_optimizer.py
@@ -7,15 +7,13 @@ def read_json(fname):
         return json.loads(file.read())
 
 def init_optimizer(config_data,data_fname):
-    scales = {name: 1e-1 for name,item in config_data['dim_ranges'].items() if item['type'] == "continuous" or item['type'] == "multiplicative"}
+    scales = {name: 1e-1 for name,item in config_data['dim_ranges'].items()}
     new_data = config_data
     new_data['data_points'] = []
     new_data['guassian_params'] = {
         "noise": 1e-2,
         "length_scales": scales
     }
-    new_data['contin_ordering'] = [name for name in scales]
-    new_data['disc_ordering'] = list(sorted([name for name,item in config_data['dim_ranges'].items() if item['type'] == "discrete"]))
 
     with open(data_fname,'w') as file:
         json.dump(new_data,file, indent=2)
--- next_point.py
+++ next_point.py
@@ -7,122 +7,122 @@ import sklearn
 import sklearn.gaussian_process
 import scipydirect
 
-def format_disc_comb(name,val):
-    return name+"_"+str(val)
+class Transformer:
+    def __init__(self,data):
+        self.data_specs = data['dim_ranges']
+        #self.length_scales = data["guassian_params"]["length_scales"]
+        self.data = data
 
-def format_combs(combs):
-    return ";".join(sorted(combs))
+        self.ordering = [name for name in self.data_specs]
+        self.transforms = self.order(self.get_transforms(data))
 
-def all_comb_helper(disc_vals,cur_comb,cur_combs):
-    if not disc_vals:
-        cur_combs.append(cur_comb)
-    else:
-        name,num = disc_vals[0]
-        for val in range(num):
-            all_comb_helper(disc_vals[1:],cur_comb+[format_disc_comb(name,val)],cur_combs)
+    def order(self,vals):
+        ordered_scales = []
+        for name in self.ordering:
+            ordered_scales.append(vals[name])
+        return ordered_scales
 
-def all_combinations(disc_vals):
-    cur_combs = []
-    all_comb_helper(disc_vals,[],cur_combs)
-    return [format_combs(v) for v in cur_combs]
+    def transform_point(self,val):
+        result = []
+        for name,trans in zip(self.ordering,self.transforms):
+            cur_val = val[name]
+            if trans['disc_val']:
+                for x in range(trans['disc_val']):
+                    lval = trans['scale'] if x == cur_val else 0
+                    result.append(lval)
+            else:
+                if trans['log']:
+                    cur_val = math.log(cur_val)
+                if trans['scale']:
+                    cur_val /= trans['scale']
+                result.append(cur_val)
 
-def order_vals(data,vals):
-    ordering = data['contin_ordering']
+        return np.asarray(result,dtype=np.float64)
 
-    ordered_scales = []
-    for name in ordering:
-        ordered_scales.append(vals[name])
-    return ordered_scales
+    def get_bounds(self):
+        result = []
+        for name,trans in zip(self.ordering,self.transforms):
+            if trans['disc_val']:
+                for x in range(trans['disc_val']):
+                    result.append(np.asarray([0,trans['scale']],dtype=np.float64))
+            else:
+                currange = np.asarray(self.data_specs[name]['range'],dtype=np.float64)
+                if trans['log']:
+                    currange = np.log(currange)
+                if trans['scale']:
+                    currange /= trans['scale']
+                result.append(currange)
 
-def get_discrete_vals(data):
-    data_specs = data['dim_ranges']
-    ordering = data['disc_ordering']
-    discrete_vals = []
+        return np.stack(result)
 
-    for name in ordering:
-        specs =  data_specs[name]
-        if specs['type'] == 'discrete':
-            num_possiblities = data_specs[name]['possibilities']
-            discrete_vals.append((name,num_possiblities))
-    return discrete_vals
+    def inverse_point(self,np_arr):
+        cur_idx = 0
+        res = dict()
+        for name,trans in zip(self.ordering,self.transforms):
+            if trans['disc_val']:
+                count = trans['disc_val']
+                indexed = [(np_arr[x+cur_idx],x) for x in range(count)]
+                best_idx = min(indexed)[1]
+                res[name] = best_idx
+                cur_idx += count
+            else:
+                cur_val = np_arr[cur_idx]
+                if trans['scale']:
+                    cur_val *= trans['scale']
+                if trans['log']:
+                    cur_val = math.exp(cur_val)
+                res[name] = cur_val
+                cur_idx += 1
 
-def transform(trans,val):
-    if trans['log']:
-        val = math.log(val)
-    if trans['scale']:
-        val /= trans['scale']
-    return val
+        return res
 
-def get_transforms(data):
-    data_specs = data['dim_ranges']
-    length_scales = data["guassian_params"]["length_scales"]
+    def get_transforms(self,data):
+        data_specs = data['dim_ranges']
+        length_scales = data["guassian_params"]["length_scales"]
 
-    transforms = dict()
-    for name in length_scales:
-        specs =  data_specs[name]
-        if specs['type'] == 'continuous':
-            start,end = specs['range']
-            scale_val = (end-start)*length_scales[name]
-            transforms[name] = {
-                "scale":scale_val,
-                "log": False
-            }
-        elif specs['type'] == 'multiplicative':
-            start,end = specs['range']
-            startl = math.log(start)
-            endl = math.log(end)
-            scale_val = (endl-startl)*length_scales[name]
-            transforms[name] = {
-                "scale":scale_val,
-                "log": True
-            }
-    return transforms
+        transforms = dict()
+        for name in length_scales:
+            specs =  data_specs[name]
+            if specs['type'] == 'continuous':
+                start,end = specs['range']
+                scale_val = (end-start)*length_scales[name]
+                transforms[name] = {
+                    "scale":scale_val,
+                    "log": False,
+                    "disc_val": 0
+                }
+            elif specs['type'] == 'multiplicative':
+                start,end = specs['range']
+                startl = math.log(start)
+                endl = math.log(end)
+                scale_val = (endl-startl)*length_scales[name]
+                transforms[name] = {
+                    "scale":scale_val,
+                    "log": True,
+                    "disc_val": 0
+                }
+            elif specs['type'] == 'discrete':
+                scale_val = length_scales[name]
+                transforms[name] = {
+                    "scale":scale_val,
+                    "log": False,
+                    "disc_val": specs['possibilities']
+                }
 
-def get_ordered_bounds(data):
-    trans = get_transforms(data)
-    data_specs = data['dim_ranges']
-    scaled_ranges = dict()
-    for name,val in data_specs.items():
-        if 'range' in val:
-            t = trans[name]
-            start,end = val['range']
-            scaled_ranges[name] = [transform(t,start),transform(t,end)]
-    return order_vals(data,scaled_ranges)
+        return transforms
 
-def get_vals_for_disc(data,discrete_label):
-    data_specs = data['dim_ranges']
-    ordering = data['contin_ordering']
-    trans = get_transforms(data)
-
-    ys = []
+def parse_data(data,trans):
     xs = []
-    for point in data['data_points']:
-        disc_label = format_combs([format_disc_comb(name,val) for name,val in point['data'].items() if data_specs[name]['type'] == "discrete" ])
-        if discrete_label == disc_label:
-            cur_y = point['reward']
-            cur_xs = {name:transform(trans[name],val) for name,val in point['data'].items() if name in ordering}
-            cur_xs = order_vals(data,cur_xs)
+    ys = []
+    for x in data['data_points']:
+        xs.append(trans.transform_point(x['data']))
+        ys.append(x['reward'])
 
-            ys.append(cur_y)
-            xs.append(cur_xs)
-
-    #npy_xs = [np.zeros([0]) for ]
-    if len(ys) == 0:
-        return np.zeros([0]) ,np.zeros([0,len(ordering)])
+    bounds = trans.get_bounds()
+    if len(xs) == 0:
+        return np.zeros([0]),np.zeros([0,len(data['dim_ranges'])]),bounds
     else:
-        xnpy = np.stack(xs)
-        ynpy = np.stack(ys)
-        return xnpy,ynpy
-
-def parse_data(data):
-    DISC_SCALE = 100
-    data_specs =  data['dim_ranges']
-
-    discrete_vals = get_discrete_vals(data)
-
-    all_combs = all_combinations(discrete_vals)
-    vals = {l:get_vals_for_disc(data,l) for l in all_combs}
-    return vals
+        return np.asarray(ys),np.stack(xs),bounds
 
 def next_point(data,xs,ys,bounds):
     if len(xs) == 0:
@@ -139,27 +139,30 @@ def next_point(data,xs,ys,bounds):
     beta = 0.2*math.log(t+1)*d #standar formula for beta
     regressor.fit(xs,ys)
     print(beta)
-    def upper_confidence_bound(x):
+    def neg_upper_confidence_bound(x):
         x = np.asarray(x)
         if len(x.shape) == 1:
             x = np.stack([x])
         mean,stdev = regressor.predict(x, return_std=True)
-        return mean + stdev * beta
+        return -(mean + stdev * beta)
     print(bounds)
     #print("bounds")
     #print(upper_confidence_bound(np.asarray([[0.00617284, 0.48765432]])))
-    min_val = scipydirect.minimize(upper_confidence_bound,bounds)
+    min_val = scipydirect.minimize(neg_upper_confidence_bound,bounds)
     xval = min_val.x
-    yval = upper_confidence_bound([xval])
+    yval = -neg_upper_confidence_bound([xval])
     return xval,yval
 
 if __name__ == "__main__":
     assert len(sys.argv) == 2 , "needs one parameter, the data filename."
     data = json.load(open(sys.argv[1]))
 
-    vals = parse_data(data)
-    print(vals)
-    xs,ys = vals['count_0']
-    bounds = get_ordered_bounds(data)
+    trans = Transformer(data)
+    ys,xs,bounds = parse_data(data,trans)
+    print(xs)
+    print(ys)
+    print(bounds)
     res = next_point(data,xs,ys,bounds)
     print(res)
+    inv_res = trans.inverse_point(res[0])
+    print(inv_res)
